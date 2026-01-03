628cd96a3f88378d26a1504c4ca507cf689ec91c
Baizid Bostami <mdbaizidbostami196@gmail.com>
2026-01-03 09:58:07 +0600

    fix some bug in my code

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -10,13 +10,13 @@ const Projects = () => {
         <div className="grid grid-cols-3 gap-5">
           {projectData.map((users) => (
 
-            <div className="bg-[#8B949E] p-5 rounded-2xl" key={users._id}>
+            <div className="bg-[#8B949E] p-5 rounded-2xl overflow-hidden" key={users._id}>
                 <div className="image-wrap">
                   <img className="rounded-xl" src={users.image} alt={users.name} />
                 </div>
 
                     <h1 className="uppercase font-bold text-2xl font-sans my-2">{users.name}</h1>
-                    <p className="capitalize font-sans">{users.details}</p>
+                    <p className="capitalize font-sans px-2">{users.details}</p>
 
                     {/* More Images */}
                       <div className="flex flex-row gap-5 overflow-hidden my-5">
